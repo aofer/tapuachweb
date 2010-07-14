88389ea8cd2479920637d2d2d806e03283db6925
Nir <Nir@Nir-PC>
2010-07-14 21:06:59 +0300

    I fixed the addMessageWindow, and now it visible!! COOL. - Amit build it, I just configed it!

--- src/java/org/tapuachForum/client/UI/MessageViewer/addMessageWindow.java
+++ src/java/org/tapuachForum/client/UI/MessageViewer/addMessageWindow.java
@@ -2,49 +2,58 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.tapuachForum.client.UI.MessageViewer;
 
+
 import com.google.gwt.user.client.ui.*;
+
 
 /**
  *
  * @author amit
  */
-public class addMessageWindow extends Composite{
+public class addMessageWindow extends Composite {
 
-    private FormPanel _mainPanel;
-    private VerticalPanel _holder;
-    private Label _header;
-    private Label _LSubject;
-    private TextBox _TBSubject;
-    private Label _LBody;
-    private TextArea _TABody;
-    private Button _BSave;
-    private Button _Bcancel;
-    private HorizontalPanel _buttonsPanel;
+    final FormPanel _mainPanel = new FormPanel();
 
-    public addMessageWindow(){
-        _mainPanel = new FormPanel();
+    public addMessageWindow() {
+      VerticalPanel _holder;
+    final Label _header;
+    final Label _LSubject;
+    final TextBox _TBSubject;
+    final Label _LBody;
+    final TextArea _TABody;
+    final Button _BSave;
+    final Button _Bcancel;
+   final HorizontalPanel _buttonsPanel;
+
+          _mainPanel.setAction("/myFormHandler");
+    _mainPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
+    _mainPanel.setMethod(FormPanel.METHOD_POST);
+
+
+     _holder = new VerticalPanel();
+    _mainPanel.setWidget(_holder);
+
         _holder = new VerticalPanel();
         _header = new Label("Add message");
         _LSubject = new Label("Subject:");
         _TBSubject = new TextBox();
         _LBody = new Label("body:");
         _TABody = new TextArea();
-        _BSave = new Button();
-        _Bcancel = new Button();
-        _mainPanel.add(_holder);
+        _BSave = new Button("Save");
+        _Bcancel = new Button("cancel");
+        _buttonsPanel = new HorizontalPanel();
         _holder.add(_header);
         _holder.add(_LSubject);
         _holder.add(_TBSubject);
         _holder.add(_LBody);
         _holder.add(_TABody);
-        _holder.add(_buttonsPanel);
+
         _buttonsPanel.add(_BSave);
         _buttonsPanel.add(_Bcancel);
-        initWidget(_mainPanel);
-
-
+        _holder.add(_buttonsPanel);
+       initWidget(_holder);
     }
-}
+}    
+
