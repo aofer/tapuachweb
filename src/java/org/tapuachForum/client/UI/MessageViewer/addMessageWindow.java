901d4b26a6f0db61a8b31ec45324e0c0bdf6ce4a
amit <amit@amit-PC>
2010-07-22 21:03:44 +0300

    the AddMessageWindow can now be used for editing and replies

--- src/java/org/tapuachForum/client/UI/MessageViewer/addMessageWindow.java
+++ src/java/org/tapuachForum/client/UI/MessageViewer/addMessageWindow.java
@@ -5,6 +5,7 @@
 package org.tapuachForum.client.UI.MessageViewer;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.BorderStyle;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -21,21 +22,21 @@ import org.tapuachForum.client.UI.ClientUser;
 public class addMessageWindow extends PopupPanel {
 
     // final PopupPanel _pp = new PopupPanel(false);
-    final FormPanel _mainPanel = new FormPanel();
+    private FormPanel _mainPanel = new FormPanel();
+    private VerticalPanel _holder;
+    private Label _header;
+    private Label _LSubject;
+    private TextBox _TBSubject;
+    private Label _LBody;
+    private TextArea _TABody;
+    private Button _BSave;
+    private Button _Bcancel;
+    private HorizontalPanel _buttonsPanel;
+    private Label lResult;/////////////////////////////test
+    private int _parentId;
+    private int _messageId;
 
-    public addMessageWindow() {
-        super(false, true);
-        final VerticalPanel _holder;
-        final Label _header;
-        final Label _LSubject;
-        final TextBox _TBSubject;
-        final Label _LBody;
-        final TextArea _TABody;
-        final Button _BSave;
-        final Button _Bcancel;
-        final HorizontalPanel _buttonsPanel;
-        final Label lResult;/////////////////////////////test
-
+    private void initComponents() {
         _mainPanel.setAction("/myFormHandler");
         _mainPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
         _mainPanel.setMethod(FormPanel.METHOD_POST);
@@ -47,8 +48,12 @@ public class addMessageWindow extends PopupPanel {
         _header = new Label("Add message");
         _LSubject = new Label("Subject:");
         _TBSubject = new TextBox();
+        _TBSubject.setWidth("500px");
         _LBody = new Label("body:");
         _TABody = new TextArea();
+        _TABody.setWidth("500px");
+        _TABody.setHeight("200px");
+
         lResult = new Label("Please enter the messege details or press cancel to go back to the Forum");
         _BSave = new Button("Save");
         _Bcancel = new Button("cancel");
@@ -64,71 +69,169 @@ public class addMessageWindow extends PopupPanel {
         _holder.add(_buttonsPanel);
         _holder.add(lResult);
         this.setGlassEnabled(true);
-        //initWidget(_holder);
-        //_pp.center();
-
-
-
-        final AsyncCallback<Integer> callback = new AsyncCallback<Integer>() {
-
-            public void onSuccess(Integer result) {
-                addMessageWindow.super.hide();
-                LayoutPanel lp = (LayoutPanel) RootLayoutPanel.get().getWidget(0);
-                lp.remove(3);
-                MessageViewer m = new MessageViewer();
-                m.setSize("1024 px", "300 px");
-                ScrollPanel s = new ScrollPanel(m);
-                s.setHeight("430px");
-                m.setStyleName("messageviewer");
-                lp.add(s);
-                lp.setWidgetTopHeight(s, 130, Unit.PX, 450, Unit.PX);
-            }
-
-            public void onFailure(Throwable caught) {
-                  _Bcancel.setEnabled(true);
-                  _Bcancel.setText("Go Back");
-       //           lResult.setText("PROBLEM!! the problem is" +  caught.getMessage());
-                lResult.setText("There was a problem to add the message. Please REFRESH the forum and try again.");
-
-       //         RootLayoutPanel.get().remove(1);
-      //          RootLayoutPanel.get().getWidget(0).setVisible(true);
-
-            }
-        };
-
-        _BSave.addClickHandler(new ClickHandler() {
-
-            public void onClick(ClickEvent event) {
-                _BSave.setEnabled(false);
-                _Bcancel.setEnabled(false);
-                String subject = _TBSubject.getText();
-                String body = _TABody.getText();
-                String _nickName = ClientUser.getClient().getNickName();
-                lResult.setStyleName("panel");
-                lResult.setText("please wait while the server adding your message.");
-                //getService().myMethod("test", callback);
-                getService().addMessage(_nickName, subject, body, callback);
-        //        addMessageWindow.super.hide();
-
-
-            }
-        });
-
-
-        _Bcancel.addClickHandler(new ClickHandler() {
-
-            public void onClick(ClickEvent event) {
-                //     _holder.setVisible(false);
-                //RootLayoutPanel.get().remove(1);
-                //RootLayoutPanel.get().getWidget(0).setVisible(true);
-                addMessageWindow.super.hide();
-            }
-        });
         this.center();
     }
+
+    public addMessageWindow() {
+        super(false, true);
+        _parentId = 0;
+        _messageId = 0;
+        initComponents();
+        _BSave.addClickHandler(addMessageHandler);
+        _Bcancel.addClickHandler(cancelHandler);
+    }
+
+    public addMessageWindow(int parentId) {
+        super(false, true);
+        initComponents();
+        _parentId = parentId;
+        _messageId = 0;
+        this._BSave.addClickHandler(replyHandler);
+        _Bcancel.addClickHandler(cancelHandler);
+
+    }
+
+    public addMessageWindow(int messageId, String subject, String body) {
+        super(false, true);
+        _parentId = 0;
+        _messageId = messageId;
+        this._TBSubject.setText(subject);
+        this._TABody.setText(body);
+        initComponents();
+        this._BSave.addClickHandler(editHandler);
+        _Bcancel.addClickHandler(cancelHandler);
+    }
+    private ClickHandler cancelHandler = new ClickHandler() {
+
+        public void onClick(ClickEvent event) {
+            //     _holder.setVisible(false);
+            //RootLayoutPanel.get().remove(1);
+            //RootLayoutPanel.get().getWidget(0).setVisible(true);
+            addMessageWindow.super.hide();
+        }
+    };
+    private ClickHandler editHandler = new ClickHandler() {
+
+        public void onClick(ClickEvent event) {
+            _BSave.setEnabled(false);
+            _Bcancel.setEnabled(false);
+            String subject = _TBSubject.getText();
+            String body = _TABody.getText();
+            lResult.setStyleName("panel");
+            lResult.setText("please wait while the server updating  the message.");
+
+            getService().editMessage(ClientUser.getClient().getNickName(), _messageId, subject, body, editCallback);
+            addMessageWindow.super.hide();
+        }
+    };
+    private ClickHandler addMessageHandler = new ClickHandler() {
+
+        public void onClick(ClickEvent event) {
+            _BSave.setEnabled(false);
+            _Bcancel.setEnabled(false);
+            String subject = _TBSubject.getText();
+            String body = _TABody.getText();
+            String _nickName = ClientUser.getClient().getNickName();
+            lResult.setStyleName("panel");
+            lResult.setText("please wait while the server adding your message.");
+            //getService().myMethod("test", callback);
+            getService().addMessage(_nickName, subject, body, callback);
+            //        addMessageWindow.super.hide();
+
+
+        }
+    };
+    private ClickHandler replyHandler = new ClickHandler() {
+
+        public void onClick(ClickEvent event) {
+            _BSave.setEnabled(false);
+            _Bcancel.setEnabled(false);
+            String subject = _TBSubject.getText();
+            String body = _TABody.getText();
+            String nickName = ClientUser.getClient().getNickName();
+            lResult.setStyleName("panel");
+            lResult.setText("please wait while the server adding your message");
+            //getService().myMethod("test", callback);
+            getService().addReply(_parentId, nickName, subject, body, replyCallback);
+            //    replyMessageWindow.super.hide();
+
+        }
+    };
 
     public static MyServiceAsync getService() {
         return GWT.create(MyService.class);
     }
+    final AsyncCallback<Integer> callback = new AsyncCallback<Integer>() {
+
+        public void onSuccess(Integer result) {
+            addMessageWindow.super.hide();
+            LayoutPanel lp = (LayoutPanel) RootLayoutPanel.get().getWidget(0);
+            lp.remove(3);
+            MessageViewer m = new MessageViewer();
+            m.setSize("1024 px", "300 px");
+            ScrollPanel s = new ScrollPanel(m);
+            s.setHeight("430px");
+            m.setStyleName("messageviewer");
+            lp.add(s);
+            lp.setWidgetTopHeight(s, 130, Unit.PX, 450, Unit.PX);
+        }
+
+        public void onFailure(Throwable caught) {
+            _Bcancel.setEnabled(true);
+            _Bcancel.setText("Go Back");
+            //           lResult.setText("PROBLEM!! the problem is" +  caught.getMessage());
+            lResult.setText("There was a problem to add the message. Please REFRESH the forum and try again.");
+
+            //         RootLayoutPanel.get().remove(1);
+            //          RootLayoutPanel.get().getWidget(0).setVisible(true);
+
+        }
+    };
+    final AsyncCallback<String> replyCallback = new AsyncCallback<String>() {
+
+        public void onSuccess(String result) {
+            addMessageWindow.super.hide();
+            LayoutPanel lp = (LayoutPanel) RootLayoutPanel.get().getWidget(0);
+            lp.remove(3);
+            MessageViewer m = new MessageViewer();
+            m.setSize("1024 px", "300 px");
+            ScrollPanel s = new ScrollPanel(m);
+            s.setHeight("430px");
+            m.setStyleName("messageviewer");
+            lp.add(s);
+            lp.setWidgetTopHeight(s, 130, Unit.PX, 450, Unit.PX);
+        }
+
+        public void onFailure(Throwable caught) {
+            _Bcancel.setEnabled(true);
+            _Bcancel.setText("Go Back");
+            //           lResult.setText("PROBLEM!! the problem is" +  caught.getMessage());
+            lResult.setText("There was a problem to add the reply. Please REFRESH the forum and try again.");
+
+        }
+    };
+    final AsyncCallback<String> editCallback = new AsyncCallback<String>() {
+
+        public void onSuccess(String result) {
+            addMessageWindow.super.hide();
+            LayoutPanel lp = (LayoutPanel) RootLayoutPanel.get().getWidget(0);
+            lp.remove(3);
+            MessageViewer m = new MessageViewer();
+            m.setSize("1024 px", "300 px");
+            ScrollPanel s = new ScrollPanel(m);
+            s.setHeight("430px");
+            m.setStyleName("messageviewer");
+            lp.add(s);
+            lp.setWidgetTopHeight(s, 130, Unit.PX, 450, Unit.PX);
+        }
+
+        public void onFailure(Throwable caught) {
+            _Bcancel.setEnabled(true);
+            _Bcancel.setText("Go Back");
+            //           lResult.setText("PROBLEM!! the problem is" +  caught.getMessage());
+            lResult.setText("There was a problem to edit the message. Please REFRESH the forum and try again.");
+
+        }
+    };
 }
 
