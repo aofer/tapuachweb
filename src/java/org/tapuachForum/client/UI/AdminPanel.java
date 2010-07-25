f0c864f3b0ee8737436193dc340a603f7ddf4ead
amit <amit@amit-PC>
2010-07-25 12:49:26 +0300

    added administrator button (for some reason it doesn't open the pop up)

--- src/java/org/tapuachForum/client/UI/AdminPanel.java
+++ src/java/org/tapuachForum/client/UI/AdminPanel.java
@@ -16,10 +16,12 @@ import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import java.util.List;
+import java.util.Vector;
 import org.tapuachForum.client.MyService;
 import org.tapuachForum.client.MyService.Locator;
 import org.tapuachForum.client.MyServiceAsync;
 import org.tapuachForum.shared.Member;
+import org.tapuachForum.shared.MemberInterface;
 import org.tapuachForum.shared.eMemberType;
 
 /**
@@ -29,33 +31,39 @@ import org.tapuachForum.shared.eMemberType;
 public class AdminPanel extends PopupPanel {
 
     final FormPanel _mainPanel = new FormPanel();
+    final VerticalPanel _holder;
+    final HorizontalPanel _buttonsPanel;
+    final ListBox _users;
+    final Button _upgrade;
+    final Button _close;
+    final Label _header;
+    private Vector<MemberInterface> _regularMembers;
 
-    public AdminPanel(String users) {
+    public AdminPanel() {
         super(false, true);
-        final VerticalPanel _holder;
-        final HorizontalPanel _buttonsPanel;
-        final ListBox _users;
-        final Button _upgrade;
-        final Button _close;
-        final Label _header;
+        _regularMembers = new Vector<MemberInterface>();
+        _upgrade = new Button("Upgrade");
+        _close = new Button("Close");
+        _header = new Label("Upgrade Users:");
+        _buttonsPanel = new HorizontalPanel();
+        _holder = new VerticalPanel();
+        _users = new ListBox();
+        getRegularMembers();
+        
         _mainPanel.setAction("/myFormHandler");
         _mainPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
         _mainPanel.setMethod(FormPanel.METHOD_POST);
         this.setWidget(_mainPanel);
-        _holder = new VerticalPanel();
+        this.setWidget(_holder);
         _holder.setStyleName("blueBack");
         _mainPanel.setWidget(_holder);
-        _buttonsPanel = new HorizontalPanel();
 
 
-        _users = new ListBox();
+
+
         _users.setSize("100px", "150px");
-        _users.addItem("Arseny");//test
-        _users.addItem("Bob");//test
         _users.setVisibleItemCount(10);
-        _upgrade = new Button("Upgrade");
-        _close = new Button("Close");
-        _header = new Label("Upgrade Users:");
+
         _holder.add(_header);
         _holder.add(_users);
 
@@ -64,20 +72,6 @@ public class AdminPanel extends PopupPanel {
         _holder.add(_buttonsPanel);
         this.setGlassEnabled(true);
 
-        getService().getMembers(new AsyncCallback<List<Member>>() {
-
-            public void onSuccess(List<Member> result) {
-                for (Member justMem : result) {
-                    if (justMem.getType() == eMemberType.member) {
-                        _users.addItem(justMem.getUserName());
-                    }
-                }
-            }
-
-            public void onFailure(Throwable caught) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        });
         _users.setItemSelected(0, true);
 
 
@@ -87,36 +81,53 @@ public class AdminPanel extends PopupPanel {
                 AdminPanel.super.hide();
             }
         });
-        this.center();
-
-        _upgrade.addClickHandler(new ClickHandler() {
-
-            public void onClick(ClickEvent event) {
-                _close.setEnabled(false);
-                _upgrade.setEnabled(false);
-                int indexChose = _users.getSelectedIndex();
-                String chose = _users.getItemText(indexChose);
-                getService().upgradeUser(chose, new AsyncCallback<String>() {
-
-                    public void onSuccess(String result) {
-                        _close.setEnabled(true);
-                        _upgrade.setEnabled(true);
-                    }
-
-                    public void onFailure(Throwable caught) {
-                         _close.setEnabled(true);
-                        _upgrade.setEnabled(true);
-                        _mainPanel.add(new Label("problem: " + caught.getMessage()));
-
-                    }
-                });
-            }
-        });
+        _upgrade.addClickHandler(new UpgradeClickHandler());
         this.center();
     }
 
     public static MyServiceAsync getService() {
 
         return Locator.getInstance();
+    }
+
+    protected class UpgradeClickHandler implements ClickHandler {
+
+        public void onClick(ClickEvent event) {
+            _close.setEnabled(false);
+            _upgrade.setEnabled(false);
+            int indexChose = _users.getSelectedIndex();
+            String chose = _users.getItemText(indexChose);
+            getService().upgradeUser(chose, new AsyncCallback<String>() {
+
+                public void onSuccess(String result) {
+                    _close.setEnabled(true);
+                    _upgrade.setEnabled(true);
+                }
+
+                public void onFailure(Throwable caught) {
+                    _close.setEnabled(true);
+                    _upgrade.setEnabled(true);
+                    _mainPanel.add(new Label("problem: " + caught.getMessage()));
+
+                }
+            });
+        }
+    }
+
+    public void getRegularMembers() {
+        getService().getMembers(new AsyncCallback<List<Member>>() {
+
+            public void onFailure(Throwable caught) {
+                //TODO status change
+            }
+
+            public void onSuccess(List<Member> result) {
+                for (Member m : result) {
+                    if (m.getType() == eMemberType.member) {
+                        _regularMembers.add(m);
+                    }
+                }
+            }
+        });
     }
 }
